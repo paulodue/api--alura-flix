afabd296e6489152b1439cb2d9e96ae35c7bddf7
Paulo Eduardo <pauloedu98@hotmail.com>
2021-09-04 18:24:39 -0300

    primeiro commit

---
+++ src/test/java/br/com/aluraflix/apialuraflix/ApiAluraFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aluraflix.apialuraflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAluraFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
